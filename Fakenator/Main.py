93166b0a5ede24c8054f90e3166d3d53055243b4
luccasf18 <luccasfigueiragc@gmail.com>
2025-03-17 18:29:22 -0300

    Project Fakenator

---
+++ Fakenator/Main.py
@@ -0,0 +1,29 @@
+import random
+
+frase = input("tente advinhar o numero de 0 a 10 em 3 tentativas, você aceita? (S/N): ")
+contador = 0
+
+if frase == "S" or frase == "s":
+    numberRandom = random.randint(0, 10)
+    print(numberRandom)
+
+    while contador < 3:
+      number = int(input("Digite um numero de 0 a 10: "))
+      print(contador)
+
+      if number == numberRandom:
+         print("Parabens!!, Voce acertou")
+         break
+      elif contador == 2:
+         print("Nao foi dessa vez, tente novamente mais tarde!")
+         break
+      else:
+         print("Tente novamente!!!")
+
+      contador = contador + 1
+      print(contador)
+
+elif frase == "N" or frase == "n":
+    print("Que pena que não quis brincar, :( ")
+else:
+    print("valor errado")
---
+++ Fakenator/README.md
@@ -0,0 +1,4 @@
+FALSENATOR
+Description of project:
+meu projeto consiste em um pequeno jogo em qual a pessoa pode aceitar com S ( para sim) e N (para não).
+a ideia é você tentar acertar em 3 alternativas um valor de 0 a 10 que vai ser gerado randomicamente.
